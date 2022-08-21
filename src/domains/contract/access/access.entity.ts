065905fe8e3bc80f9faff71101dd4d6c740cce36
arthurpieri <git@arthurpieri.com>
2022-08-21 17:05:33 -0300

    feat: creating access entity and contract types

---
+++ src/domains/contract/access/access.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity } from 'typeorm'
+
+@Entity()
+export class Access {
+  @Column()
+  id: string
+
+  @Column()
+  system: string
+
+  @Column()
+  login_tret: string
+
+  @Column()
+  pass_tret: string
+
+  @Column()
+  login_client: string
+
+  @Column()
+  pass_client: string
+
+  @Column()
+  contract: string
+}
---
+++ src/domains/contract/common/contract.types.ts
@@ -0,0 +1,9 @@
+export interface IAccess {
+  id?: string
+  system: string
+  login_tret?: string
+  pass_tret?: string
+  login_client: string
+  pass_client: string
+  contract: string
+}
