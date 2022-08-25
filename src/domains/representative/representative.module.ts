a258936dd303e1f9e2d7cc2e4e94a37f18c5f1d3
arthurpieri <git@arthurpieri.com>
2022-08-25 01:07:27 -0300

    feat: representative module and import in app.module

--- src/domains/representative/representative.module.ts
+++ src/domains/representative/representative.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
-import { AccessService } from './representative.service'
-import { AccessController } from './representative.controller'
+import { RepresentativeService } from './representative.service'
+import { RepresentativeController } from './representative.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { AccessRepository } from './representative.entity'
+import { RepresentativeRepository } from './representative.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AccessRepository])],
-  controllers: [AccessController],
-  providers: [AccessService],
+  imports: [TypeOrmModule.forFeature([RepresentativeRepository])],
+  controllers: [RepresentativeController],
+  providers: [RepresentativeService],
 })
-export class AccessModule {}
+export class RepresentativeModule {}
