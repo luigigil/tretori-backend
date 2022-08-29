2f8870c110d5892d195d26e01df47fab6b282e0d
arthurpieri <git@arthurpieri.com>
2022-08-29 12:18:51 -0300

    fix: imports

--- src/domains/core/contract/test/contract.controller.spec.ts
+++ src/domains/core/contract/test/contract.controller.spec.ts
@@ -1,9 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { LegalPerson } from '../../../customer/legal-person/legal-person.entity'
 import { LegalPersonService } from '../../../customer/legal-person/legal-person.service'
+import {
+  legalPersonArrayFixture,
+  oneLegalPersonFixture,
+} from '../../../customer/legal-person/test/fixtures'
+import { PhysicalPerson } from '../../../customer/physical-person/physical-person.entity'
 import { PhysicalPersonService } from '../../../customer/physical-person/physical-person.service'
+import {
+  onePhysicalPersonFixture,
+  physicalPersonArrayFixture,
+} from '../../../customer/physical-person/test/fixtures'
+import { Access } from '../../access/access.entity'
+import { oneAccessFixture } from '../../access/access.fixtures'
 import { AccessService } from '../../access/access.service'
+import { Move } from '../../move/move.entity'
 import { MoveService } from '../../move/move.service'
+import { moveArrayFixture, oneMoveFixture } from '../../move/test/fixtures'
+import { Renew } from '../../renew/renew.entity'
 import { RenewService } from '../../renew/renew.service'
+import { oneRenewFixture, renewArrayFixture } from '../../renew/test/fixtures'
 import { ContractController } from '../contract.controller'
 import { ContractService } from '../contract.service'
 import { IContract } from '../contract.types'
@@ -38,6 +55,59 @@ describe('ContractController', () => {
             remove: jest.fn(),
             update: jest.fn(),
             moveContract: jest.fn().mockResolvedValue(oneContractFixture),
+          },
+        },
+        {
+          provide: getRepositoryToken(Move),
+          useValue: {
+            find: jest.fn().mockResolvedValue(moveArrayFixture),
+            findOneBy: jest.fn().mockResolvedValue(oneMoveFixture),
+            save: jest.fn().mockResolvedValue(oneMoveFixture),
+            remove: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(Renew),
+          useValue: {
+            find: jest.fn().mockResolvedValue(renewArrayFixture),
+            findOneBy: jest.fn().mockResolvedValue(oneRenewFixture),
+            save: jest.fn().mockResolvedValue(oneRenewFixture),
+            remove: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(LegalPerson),
+          useValue: {
+            find: jest.fn().mockResolvedValue(legalPersonArrayFixture),
+            findOneBy: jest.fn().mockResolvedValue(oneLegalPersonFixture),
+            save: jest.fn().mockResolvedValue(oneLegalPersonFixture),
+            remove: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(PhysicalPerson),
+          useValue: {
+            find: jest.fn().mockResolvedValue(physicalPersonArrayFixture),
+            findOneBy: jest.fn().mockResolvedValue(onePhysicalPersonFixture),
+            save: jest.fn().mockResolvedValue(onePhysicalPersonFixture),
+            remove: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(Access),
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation(() => Promise.resolve({ id: 1, ...oneAccessFixture })),
+            findOne: jest
+              .fn()
+              .mockImplementation((id: number) => Promise.resolve({ id, ...oneAccessFixture })),
+            remove: jest.fn(),
+            update: jest.fn(),
           },
         },
       ],
