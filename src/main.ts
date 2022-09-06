eda6739fba12818b2276bbe8413cc03b101174b6
arthurpieri <git@arthurpieri.com>
2022-09-06 15:37:13 -0300

    fix: adding change

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
+// só pra ter uma mudança
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
 
