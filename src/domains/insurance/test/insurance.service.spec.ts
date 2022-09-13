788f426e21156cabc07b4b047040c060575ca749
arthurpieri <git@arthurpieri.com>
2022-09-13 03:12:50 -0300

    fix: insurance.service tests

--- src/domains/insurance/test/insurance.service.spec.ts
+++ src/domains/insurance/test/insurance.service.spec.ts
@@ -7,8 +7,8 @@ import { InsuranceService } from '../insurance.service'
 import { insuranceArrayFixture, oneInsuranceFixture } from './fixtures/index'
 
 describe('InsuranceService', () => {
-  let service: InsuranceService
-  let repository: Repository<Insurance>
+  let insuranceService: InsuranceService
+  let insuranceRepository: Repository<Insurance>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,55 +20,66 @@ describe('InsuranceService', () => {
             find: jest.fn().mockResolvedValue(insuranceArrayFixture),
             findOneBy: jest.fn().mockResolvedValue(oneInsuranceFixture),
             save: jest.fn().mockResolvedValue(oneInsuranceFixture),
-            remove: jest.fn(),
-            delete: jest.fn(),
+            remove: jest
+              .fn()
+              .mockResolvedValueOnce({ id: 1, ...oneInsuranceFixture })
+              .mockRejectedValueOnce(() => {
+                throw new NotFoundException('Insurance not found')
+              }),
           },
         },
       ],
     }).compile()
 
-    service = module.get<InsuranceService>(InsuranceService)
-    repository = module.get<Repository<Insurance>>(getRepositoryToken(Insurance))
+    insuranceService = module.get<InsuranceService>(InsuranceService)
+    insuranceRepository = module.get<Repository<Insurance>>(getRepositoryToken(Insurance))
   })
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
+    expect(insuranceService).toBeDefined()
   })
 
   describe('create()', () => {
     it('should successfully insert a insurance', async () => {
-      await expect(service.create(oneInsuranceFixture)).resolves.toEqual(oneInsuranceFixture)
+      await expect(insuranceService.create(oneInsuranceFixture)).resolves.toEqual(
+        oneInsuranceFixture
+      )
     })
   })
 
   describe('findAll()', () => {
     it('should return an array of insurance', async () => {
-      const insuranceArray = await service.findAll()
+      const insuranceArray = await insuranceService.findAll()
       expect(insuranceArray).toEqual(insuranceArrayFixture)
     })
   })
 
   describe('findOne()', () => {
     it('should get a single insurance', async () => {
-      const repoSpy = jest.spyOn(repository, 'findOneBy')
-      await expect(service.findOne(1)).resolves.toEqual(oneInsuranceFixture)
+      const repoSpy = jest.spyOn(insuranceRepository, 'findOneBy')
+      await expect(insuranceService.findOne(1)).resolves.toEqual(oneInsuranceFixture)
       expect(repoSpy).toHaveBeenCalledWith({ id: 1 })
     })
     it('should throw NotFoundException if no insurance is found', async () => {
-      const repoSpy = jest.spyOn(repository, 'findOneBy')
+      const repoSpy = jest.spyOn(insuranceRepository, 'findOneBy')
       repoSpy.mockResolvedValue(null)
-      await expect(service.findOne(1)).rejects.toThrow(new NotFoundException('Insurance not found'))
+      await expect(insuranceService.findOne(1)).rejects.toThrow(
+        new NotFoundException('Insurance not found')
+      )
     })
   })
 
   describe('remove()', () => {
     it('should call remove with the passed value', async () => {
-      const removeSpy = jest.spyOn(repository, 'remove')
-      await expect(service.remove(2)).resolves.toBeDefined()
+      const removeSpy = jest.spyOn(insuranceRepository, 'remove')
+      await expect(insuranceService.remove(2)).resolves.toBeDefined()
       expect(removeSpy).toBeDefined()
     })
     it('should throw NotFoundException if no insurance is found', async () => {
-      await expect(service.remove(99)).rejects.toThrow(new NotFoundException('Insurance not found'))
+      await insuranceService.remove(1)
+      await expect(insuranceService.remove(99)).rejects.toThrow(
+        new NotFoundException('Insurance not found')
+      )
     })
   })
 })
